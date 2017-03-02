6597caf71333bfe4f3ca8e357601116931318d2f
Joel Cervantes <joecrvnts@gmail.com>
2017-03-02 22:36:26 +0000

    fix mistakes on articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was succesfully created"
-      redirect_to article_path(article)
+      redirect_to article_path(@article)
     else
       render 'new'
     end
